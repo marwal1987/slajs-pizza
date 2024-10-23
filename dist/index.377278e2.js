2edc15bfc308d91a4c2d9c760c10461c7cfad3be
marwal1987 <144460382+marwal1987@users.noreply.github.com>
2024-10-23 14:23:46 +0200

    inital commit

---
+++ dist/index.377278e2.js
@@ -0,0 +1,5 @@
+const appDiv = document.getElementById("app");
+appDiv.innerHTML = `<h1>Welcome to Slajs Pizza Menu</h1>`;
+let pizzaName = "Margherita";
+
+//# sourceMappingURL=index.377278e2.js.map
