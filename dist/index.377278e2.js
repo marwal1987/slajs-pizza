b38d61e0a0b16c66e2b882d98b286d10053319dd
marwal1987 <144460382+marwal1987@users.noreply.github.com>
2024-10-23 14:41:54 +0200

    removed parvel-cache och dist

--- dist/index.377278e2.js
+++
@@ -1,5 +0,0 @@
-const appDiv = document.getElementById("app");
-appDiv.innerHTML = `<h1>Welcome to Slajs Pizza Menu</h1>`;
-let pizzaName = "Margherita";
-
-//# sourceMappingURL=index.377278e2.js.map
