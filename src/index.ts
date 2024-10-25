21e4edb5eacacc1de39b17ef60c1b96ecdd8beb3
Rebecka <rebeckagamble@hotmail.com>
2024-10-26 01:33:20 +0200

    show fetched data

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,22 @@ const pizzas: Pizza[] = [];
 const salads: Salad[] = [];
 const drinks: Drink[] = [];
 
+//func for each item, contains diff data
+function isPizza(item: Pizza | Salad | Drink): item is Pizza {
+  return (item as Pizza).toppings !== undefined;
+}
+
+function isSalad(item: Pizza | Salad | Drink): item is Salad {
+  return (item as Salad).ingredients !== undefined;
+}
+
+function isDrink(item: Pizza | Salad | Drink): item is Drink {
+  return (
+    (item as Drink).description !== undefined &&
+    (item as Pizza).toppings === undefined
+  );
+}
+
 async function createMenu() {
   const menuItems = await fetchMenu();
   if (menuItems) {
@@ -33,6 +49,40 @@ async function createMenu() {
       "Drinks: ",
       drinks
     );
+    showMenu("pizzas", pizzas);
+    showMenu("salads", salads);
+    showMenu("drinks", drinks);
+  }
+}
+
+function showMenu(sectionId: string, items: (Pizza | Salad | Drink)[]) {
+  const section = document.getElementById(sectionId);
+
+  if (section) {
+    items.forEach((item) => {
+      const itemEl = document.createElement("div");
+      itemEl.classList.add("menuItem");
+
+      let content = "";
+      if (isPizza(item)) {
+        content = `<p>${item.description}</p>`;
+        content += `<p><b>Toppings:</b> ${item.toppings.join(", ")}</p>`;
+      } else if (isSalad(item)) {
+        content = `<p>${item.ingredients.join(", ")}</p>`;
+      } else if(isDrink(item)) {
+        content = `<p>${item.description}</p>`;
+      }
+
+      itemEl.innerHTML = `
+          <h3>${item.name} - ${item.price} SEK</h3>
+          <div class="imgContainer">
+            <img src="${item.imgUrl}" alt="${item.name}" />
+          </div>
+          <p>${content}</p>
+        `;
+
+      section.appendChild(itemEl);
+    });
   }
 }
 
